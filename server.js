214064a1b5726d9ab2a4f63038f61ff63b35e637
Bermillo <reginald.j.bermillo@accenture.com>
2022-12-22 17:55:29 +0800

    test

--- server.js
+++ server.js
@@ -11,6 +11,30 @@ app.use(createProxyMiddleware({
   logger: console
 }))
 
+const updateQueryStringParameter = (path, key, value) => {
+  const re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+  const separator = path.indexOf('?') !== -1 ? '&' : '?';
+  if (path.match(re)) {
+    return path.replace(re, '$1' + key + '=' + value + '$2');
+  } else {
+    return path + separator + key + '=' + value;
+  }
+};
+
+const options = {
+  changeOrigin: true,
+  logger: console,
+  router: (req) => new URL(req.path.substring(1)),
+  pathRewrite: function(path, req) {
+    let newPath = path;
+    newPath = updateQueryStringParameter(newPath, 'mediaFormats',  'WAV');
+    // newPath = updateQueryStringParameter(newPath, 'client_secret',  clientSecret);
+    return newPath;
+  },
+};
+
+app.use('/audio', proxy(options));
+
 app.listen(process.env.PORT || 8088, () => {
   console.info('proxy server is running on port 8088')
 })
