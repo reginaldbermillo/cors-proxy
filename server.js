3db1860db0d3ce0fae85cbde7385309fcab7300d
Bermillo <reginald.j.bermillo@accenture.com>
2023-01-20 16:26:40 +0800

    test add params

--- server.js
+++ server.js
@@ -6,42 +6,36 @@ const app = express()
 app.use(cors())
 app.use(createProxyMiddleware({
   router: (req) => new URL(req.path.substring(1)),
-  pathRewrite: function(path, req) {
-    let newPath = new URL(req.path.substring(1)).pathname;
-    newPath = updateQueryStringParameter(newPath, 'mediaFormats',  'WAV');
-    newPath = updateQueryStringParameter(newPath, 'expand',  'evaluationForm');
-    newPath = updateQueryStringParameter(newPath, 'expand',  'agent');
-    newPath = updateQueryStringParameter(newPath, 'expand',  'evaluator');
-    // newPath = updateQueryStringParameter(newPath, 'client_secret',  clientSecret);
-    return newPath;
-  },
+  pathRewrite: (path, req) => (new URL(req.path.substring(1))).pathname,
   changeOrigin: true,
   logger: console
 }))
-
-const updateQueryStringParameter = (path, key, value) => {
-  const re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
-  const separator = path.indexOf('?') !== -1 ? '&' : '?';
-  if (path.match(re)) {
-    return path.replace(re, '$1' + key + '=' + value + '$2');
-  } else {
-    return path + separator + key + '=' + value;
-  }
-};
-
-// const options = {
-//   changeOrigin: true,
-//   logger: console,
+// app.use(createProxyMiddleware({
 //   router: (req) => new URL(req.path.substring(1)),
 //   pathRewrite: function(path, req) {
 //     let newPath = new URL(req.path.substring(1)).pathname;
 //     newPath = updateQueryStringParameter(newPath, 'mediaFormats',  'WAV');
+//     newPath = updateQueryStringParameter(newPath, 'expand',  'evaluationForm');
+//     newPath = updateQueryStringParameter(newPath, 'expand',  'agent');
+//     newPath = updateQueryStringParameter(newPath, 'expand',  'evaluator');
 //     // newPath = updateQueryStringParameter(newPath, 'client_secret',  clientSecret);
 //     return newPath;
+//   },
+//   changeOrigin: true,
+//   logger: console
+// }))
+
+// const updateQueryStringParameter = (path, key, value) => {
+//   const re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
+//   const separator = path.indexOf('?') !== -1 ? '&' : '?';
+//   if (path.match(re)) {
+//     return path.replace(re, '$1' + key + '=' + value + '$2');
+//   } else {
+//     return path + separator + key + '=' + value;
 //   }
 // };
 
-// app.use('/audio', createProxyMiddleware(options));
+
 
 app.listen(process.env.PORT || 8088, () => {
   console.info('proxy server is running on port 8088')
