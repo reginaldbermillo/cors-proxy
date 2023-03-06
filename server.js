cfcf5bf09b821cf58a575a8948f5d1267b1a74fc
Bermillo <reginald.j.bermillo@accenture.com>
2023-03-07 00:58:30 +0800

    Add params

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ app.use(createProxyMiddleware({
     let formatId = req.query.formatId
     let download = req.query.download
     let fileName = req.query.fileName
+    let divisionId = req.query.divisionId
     newPath = updateQueryStringParameter(newPath, 'mediaFormats', mediaFormats);
     newPath = updateQueryStringParameter(newPath, 'expand', expandVal);
     newPath = updateQueryStringParameter(newPath, 'maxWaitMs', 5000);
     newPath = updateQueryStringParameter(newPath, 'formatId', formatId);
     newPath = updateQueryStringParameter(newPath, 'download', true);
     newPath = updateQueryStringParameter(newPath, 'fileName', fileName);
+    newPath = updateQueryStringParameter(newPath, 'divisionId', divisionId);
     // newPath = updateQueryStringParameter(newPath, 'expand',  'agent');
     // newPath = updateQueryStringParameter(newPath, 'expand',  'evaluator');
     // newPath = updateQueryStringParameter(newPath, 'client_secret',  clientSecret);
